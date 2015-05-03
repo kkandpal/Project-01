030fe9692775aa423f36a0e318f463f9f56740f2
kkandpal <ashokupcl1962@yahoo.com>
2015-05-04 01:17:57 +0530

    Create connection.php

---
+++ connection.php
@@ -0,0 +1,15 @@
+<?php
+
+$host = "localhost" ;
+
+$user = "root" ;
+
+$password = "" ;
+
+$database = "anshul" ;
+
+//For Estabilishing a Connection with the mysql database
+
+$connect = new mysqli($host , $user , $password , $database ) ;
+
+?>
