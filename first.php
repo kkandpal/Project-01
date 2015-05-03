8d08c9f27cf3dc8ea967087a3f10cf179f954973
kkandpal <ashokupcl1962@yahoo.com>
2015-05-04 02:05:22 +0530

    Create first.php

---
+++ first.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+<?php
+
+$db_hostname='localhost';
+$db_database='project';
+$db_username='root';
+$db_password='';
+
+$connect=new mysqli($db_hostname, $db_username, $db_password, $db_database);
+?>
+</body>
+</html>
